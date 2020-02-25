12ff95644347ae2a5cea259174276415c67c847a
Alex <alexolthoff@tutanota.com>
2020-02-25 16:29:13 +0800

    Learned a little about jquery effects

---
+++ Project3/js/clickEvent.js
@@ -0,0 +1,14 @@
+//$("#box").show()
+$("#target").on("click", function () {
+  //The code will hide something in the page
+  //$("p#statement").hide();
+  //$("#box").toggle();
+  //$("#box").slideDown(1000);
+  //$("#box").slideUp(1000);
+  //$("#box").slideToggle(1000);
+  //$("#box").fadeIn(1000)
+  //$("#box").fadeOut()
+  // $("#box").fadeTo(1000, 0.5)
+  // $("#box").fadeToggle(1000);
+  $("#box").
+});
