c61b93c4778a530908da7c683d681c2897c19434
Alex <alexolthoff@tutanota.com>
2020-02-26 18:45:24 +0800

    More selectors

--- Project10/js/jquery_custom_code.js
+++ Project10/js/jquery_custom_code.js
@@ -3,5 +3,22 @@ $(function () {
     $('#steps li:first, #nested-list li:first').css("color", "red")
     $("#steps li:nth-child(3)").css("border", "1px solid green");
     $("p[class]").css("border", "1px solid red");
-    $("ul[id='steps']").hide()
+    $("ul[id='steps']").hide();
+    $("ul#steps + div").css("border", "1px dotted red")
+    $("p[class!='instruct_title']")
+    // Starts with
+    $("[class^='imp'");
+    // Ends With
+    $("[class$='steps'");
+    // includes, only perfect match
+    $("[id='closing_statements'");
+    //Includes this fragment 
+    $("[id*='osing'");
+    // any div that has a p
+    $("div:has(p)");
+    // h1 h2 ect
+    $(":header");
+    // Things that don't have something
+    $("li:not('.important'");
+    $("div:not(:has(:header))")
 });
